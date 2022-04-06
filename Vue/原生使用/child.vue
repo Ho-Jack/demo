c0acb9c94b3b1879891b56127087edbc2b846271
Ho-Jack <965389230@qq.com>
2022-04-06 17:30:06 +0800

    vue3 在html引入

---
+++ Vue/原生使用/child.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">Hello {{ who }}</div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+module.exports = {
+
+const a = 1
+const b = ref(2)
+defineExpose({
+        a,
+        b
+    })
+}
+</script>
+
+<style>
+</style>
